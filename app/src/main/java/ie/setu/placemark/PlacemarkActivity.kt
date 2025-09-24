83baff2b68887840b8270894ec13a17ae6be4e2f
SineadCleary <20102299@mail.wit.ie>
2025-09-24 20:22:08 +0100

    Lab-A00 completed.

---
+++ app/src/main/java/ie/setu/placemark/PlacemarkActivity.kt
@@ -0,0 +1,19 @@
+package ie.setu.placemark
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import timber.log.Timber
+import timber.log.Timber.i
+
+class PlacemarkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_placemark)
+
+        Timber.plant(Timber.DebugTree())
+        i("Placemark activity started...")
+    }
+}
