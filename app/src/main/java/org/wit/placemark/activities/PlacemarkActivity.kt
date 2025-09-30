545fa5cdb70d24cfad0f9baed4aa2d916084f7c0
SineadCleary <20102299@mail.wit.ie>
2025-09-30 21:00:16 +0100

    Add placemark title and description.

---
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -0,0 +1,42 @@
+package org.wit.placemark.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import org.wit.placemark.databinding.ActivityPlacemarkBinding
+import org.wit.placemark.models.PlacemarkModel
+import timber.log.Timber
+import timber.log.Timber.i
+
+class PlacemarkActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPlacemarkBinding
+    var placemark = PlacemarkModel()
+    val placemarks = ArrayList<PlacemarkModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityPlacemarkBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Timber.plant(Timber.DebugTree())
+
+        i("Placemark activity started...")
+
+        binding.btnAdd.setOnClickListener() {
+            placemark.title = binding.placemarkTitle.text.toString()
+            placemark.description = binding.placemarkDescription.text.toString()
+            if (placemark.title.isNotEmpty()) {
+                i("add button pressed: ${placemark.title}, ${placemark.description}")
+                placemarks.add(PlacemarkModel(placemark.title, placemark.description))
+                i("PMs: $placemarks")
+            }
+            else {
+                Snackbar
+                    .make(it,"Please enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
+    }
+}
