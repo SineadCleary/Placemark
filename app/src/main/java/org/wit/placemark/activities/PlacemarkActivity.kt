42e78018148bafec0f2b15e4e64816fa2bf19b3e
SineadCleary <sineadcleary9@gmail.com>
2025-11-05 10:12:05 +0000

    Interfaces lab

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -4,58 +4,69 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.snackbar.Snackbar
 import org.wit.placemark.R
 import org.wit.placemark.databinding.ActivityPlacemarkBinding
 import org.wit.placemark.main.MainApp
 import org.wit.placemark.models.PlacemarkModel
-import timber.log.Timber
 import timber.log.Timber.i
 
 class PlacemarkActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPlacemarkBinding
     var placemark = PlacemarkModel()
     lateinit var app : MainApp
+    var edit = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlacemarkBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.toolbarAdd.title = title
+        setSupportActionBar(binding.toolbarAdd)
+
         app = application as MainApp
         i("Placemark activity started...")
+
+        if (intent.hasExtra("placemark_edit")) {
+            edit = true
+            placemark = intent.extras?.getParcelable("placemark_edit")!!
+            binding.placemarkTitle.setText(placemark.title)
+            binding.placemarkDescription.setText(placemark.description)
+            binding.btnAdd.setText(R.string.save_placemark)
+        }
+
         binding.btnAdd.setOnClickListener() {
             placemark.title = binding.placemarkTitle.text.toString()
             placemark.description = binding.placemarkDescription.text.toString()
-            if (placemark.title.isNotEmpty()) {
-                app.placemarks.add(placemark.copy())
-                i("add button pressed: ${placemark}")
-                for (i in app.placemarks.indices)
-                {
-                    i("Placemark[$i]:${this.app.placemarks[i]}")
-                }
-                setResult(RESULT_OK)
-                finish()
-            }
-            else {
-                Snackbar
-                    .make(it,"Please enter a title", Snackbar.LENGTH_LONG)
+            if (placemark.title.isEmpty()) {
+                Snackbar.make(it,R.string.placemark_addTitle, Snackbar.LENGTH_LONG)
                     .show()
+            } else {
+                if (edit) {
+                    app.placemarks.update(placemark.copy())
+                } else {
+                    app.placemarks.create(placemark.copy())
+                }
             }
+            setResult(RESULT_OK)
+            finish()
         }
 
         setSupportActionBar(binding.toolbarAdd)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_add, menu)
+        menuInflater.inflate(R.menu.menu_placemark, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        setResult(RESULT_OK)
-        finish()
+        when (item.itemId) {
+            R.id.item_cancel -> {
+                finish()
+            }
+        }
         return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
@@ -1,24 +1,22 @@
 package org.wit.placemark.activities
 
-import PlacemarkAdapter
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import org.wit.placemark.R
+import org.wit.placemark.adapters.PlacemarkAdapter
+import org.wit.placemark.adapters.PlacemarkListener
 import org.wit.placemark.databinding.ActivityPlacemarkListBinding
-import org.wit.placemark.databinding.CardPlacemarkBinding
 import org.wit.placemark.main.MainApp
 import org.wit.placemark.models.PlacemarkModel
 
-class PlacemarkListActivity : AppCompatActivity() {
+class PlacemarkListActivity : AppCompatActivity(), PlacemarkListener {
+
     lateinit var app: MainApp
     private lateinit var binding: ActivityPlacemarkListBinding
 
@@ -26,14 +24,14 @@ class PlacemarkListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPlacemarkListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.toolbar.title = title
+        setSupportActionBar(binding.toolbar)
 
         app = application as MainApp
 
         val layoutManager = LinearLayoutManager(this)
         binding.recyclerView.layoutManager = layoutManager
-        binding.recyclerView.adapter = PlacemarkAdapter(app.placemarks)
-        binding.toolbar.title = title
-        setSupportActionBar(binding.toolbar)
+        binding.recyclerView.adapter = PlacemarkAdapter(app.placemarks.findAll(),this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -55,11 +53,25 @@ class PlacemarkListActivity : AppCompatActivity() {
         registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
         ) {
-            if (it.resultCode == Activity.RESULT_OK) {
+            if (it.resultCode == RESULT_OK) {
                 (binding.recyclerView.adapter)?.
-                notifyItemRangeChanged(0,app.placemarks.size)
+                notifyItemRangeChanged(0,app.placemarks.findAll().size)
+            }
+        }
+
+    override fun onPlacemarkClick(placemark: PlacemarkModel) {
+        val launcherIntent = Intent(this, PlacemarkActivity::class.java)
+        launcherIntent.putExtra("placemark_edit", placemark)
+        getClickResult.launch(launcherIntent)
+    }
+
+    private val getClickResult =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            if (it.resultCode == RESULT_OK) {
+                (binding.recyclerView.adapter)?.
+                notifyItemRangeChanged(0,app.placemarks.findAll().size)
             }
         }
 }
-
-
